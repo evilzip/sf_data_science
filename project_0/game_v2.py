846434ff76ca127d4aab87fb41a721a2f14f9fa2
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-10-11 00:02:24 +0300

    Pepe8 formatting

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -2,7 +2,8 @@
 Компьютер сам загадывает и сам угадывает число"""
 import numpy as np
 
-def random_predict(number:int=1)->int:
+
+def random_predict(number: int = 1) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -11,16 +12,17 @@ def random_predict(number:int=1)->int:
     Returns:
         int: Число попыток
     """
-    
+
     count = 0
     while True:
-        count+=1
-        predict_number = np.random.randint(1,101)# пердполагаемое число
+        count += 1
+        predict_number = np.random.randint(1, 101)  # пердполагаемое число
         if number == predict_number:
-            break# выход из цикла если угадали        
+            break  # выход из цикла если угадали
     return count
 
-def smart_predict(number:int)->int:
+
+def smart_predict(number: int) -> int:
     """Угадывает число методом деления диапазона поиска пополам
 
     Args:
@@ -30,21 +32,23 @@ def smart_predict(number:int)->int:
         int: число попыток
     """
     count = 0
-    max_number = 101#начальный "правая" граница диапазона поиска 
-    min_number = 0# начальная "левая граница" диапазона поискаc
+    max_number = 101  # начальный "правая" граница диапазона поиска
+    min_number = 0  # начальная "левая граница" диапазона поискаc
     while True:
         count += 1
-        if number == (max_number+min_number) // 2:# проверяем лежит ли искомое число посередине текущего диапазона
+        # проверяем лежит ли искомое число посередине текущего диапазона
+        if number == (max_number+min_number) // 2:
             break
-        elif number<(max_number+min_number) // 2:#если число меньше центрального значения диапазона, то сдвигаем правую границу влево на половину
-            max_number=(max_number+min_number) // 2 #новое значение правой границы нового дипазона поиска
-        elif number>(max_number+min_number) // 2: #если число больше центрального значения диапазона, то сдвигаем левую границу вправо на половину
-            min_number = (max_number+min_number) // 2 #новое значени левой границы нового диапазона поиска
+        # если число меньше центрального значения диапазона, то сдвигаем правую границу влево на половину
+        elif number < (max_number+min_number) // 2:
+            # новое значение правой границы нового дипазона поиска
+            max_number = (max_number+min_number) // 2
+        # если число больше центрального значения диапазона, то сдвигаем левую границу вправо на половину
+        elif number > (max_number+min_number) // 2:
+            # новое значени левой границы нового диапазона поиска
+            min_number = (max_number+min_number) // 2
     return count
-                
-            
-    
-    
+
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 1000 подхов угадывает наш алгоритм
@@ -56,9 +60,10 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    np.random.seed(1)# Фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101,  size=(1000)) # Загадали список чисел
-    
+    np.random.seed(1)  # Фиксируем сид для воспроизводимости
+    random_array = np.random.randint(
+        1, 101,  size=(1000))  # Загадали список чисел
+
     for number in random_array:
         count_ls.append(random_predict(number))
 
@@ -66,7 +71,7 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за {score} попыток')
     return score
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     print(score_game(random_predict))
     print(score_game(smart_predict))
-
